1c4ea5281c9ebf886b673e55ffc8e90c8eab3f7c
veronicah <mbuviveronicah3@gmail.com>
2023-03-13 21:18:11 +0300

    Completed assinment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // Write your code here!iif
+  let message;
+  if (feet<=400){
+    message ='This one is on me!'
+    return message;
+  }
+  {
+    if (feet>2000 && feet <2500){
+      message ='I will gladly take your thirty bucks.'
+      return message;
+    }
+    {if (feet>2500){
+      message='No can do.'
+      return message;
+    }
+
+    }
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+  // Write your code here
+if (City==='NYC'){
+  return "Ok, sounds good."}
+  else {return "No go."}
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
+  switch(Tip){
+    case'generous':
+    return 'Thank you so much.'
+    case'not as generous':
+    return 'Thank you.'
+    default:
+      return `Bye.`
+
+  }
   // Write your code here!
 }
